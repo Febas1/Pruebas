8d0ce7c16435c2b27a127a713cc829646c3122da
jorge alejandro <alejandro-rdz-@hotmail.com>
2018-12-10 11:28:04 -0500

    Modificaciones en lectrura de datagridf

--- XmlPrueba/Prueba.xaml.cs
+++ XmlPrueba/Prueba.xaml.cs
@@ -12,6 +12,7 @@ namespace XmlPrueba
     /// </summary>
     public partial class Prueba : Window
     {
+        public object iva = new object();
         public string ruta;
         public string second;
         DataTable dt = new DataTable();
@@ -33,7 +34,6 @@ namespace XmlPrueba
             dt.Columns.Add("Valor unitario");
             dt.Columns.Add("Tipo impuesto");
             dt.Columns.Add("% Impuesto");
-            dt.Columns.Add("Impuesto");
             dt.Columns.Add("Descuento");
             dt.Columns.Add("Valor Total");
         }
@@ -41,20 +41,19 @@ namespace XmlPrueba
         private void Llenar()
         {
             dt.Clear();
-
-            int cont = 0, con1 = 0, con2 = 0, con3 = 0, con4 = 0,con9 = 0, conFina = 0;
+            int cont = 0, con1 = 0, con2 = 0, con3 = 0, con4 = 0, con9 = 0, conFina = 0;
             XElement xelement = XElement.Load(ruta);
 
             var unique = from el in xelement.Elements(fe + "InvoiceLine") select el;
-            var sub = from el in xelement.Elements(fe + "TaxTotal").Elements().Elements() select el;            
+            var sub = from el in xelement.Elements(fe + "TaxTotal").Elements().Elements() select el;
             var codigo = from el in xelement.Elements(fe + "InvoiceLine").Descendants(cbc + "ID") select el;
             var cantidad = from el in xelement.Elements(fe + "InvoiceLine").Elements(cbc + "InvoicedQuantity") select el;
             var description = from el in xelement.Elements(fe + "InvoiceLine").Elements(fe + "Item").Elements(cbc + "Description") select el;
             var valUnit = from el in xelement.Elements(fe + "InvoiceLine").Elements(fe + "Price").Elements(cbc + "PriceAmount") select el;
-            var IVA = from el in xelement.Elements(fe + "TaxTotal").Elements(fe + "TaxSubtotal").Elements(cbc+"Percent") select el;
-            object iva = new object();
-            var total = from el in xelement.Elements(fe + "InvoiceLine").Elements(cbc + "LineExtensionAmount") select el;
-
+            var IVA = from el in xelement.Elements(fe + "TaxTotal").Elements(fe + "TaxSubtotal").Elements(cbc + "Percent") select el;
+            var valTot = from el in xelement.Elements(fe + "InvoiceLine").Elements(cbc + "LineExtensionAmount") select el;
+            var totalPago = xelement.Descendants(cbc + "PayableAmount");
+                                             
             foreach (var el in unique)
             {
                 cont += 1;
@@ -64,13 +63,12 @@ namespace XmlPrueba
             object[] cantidadArrray = new object[cont];
             object[] descripcionArray = new object[cont];
             object[] valunitArray = new object[cont];
-            object[] totalArray = new object[cont];
-
+            object[] totArray = new object[cont];
             foreach (var item in codigo)
             {
-                //string[] textos = item.Value.Split('_');
-                //codigoArray[con1] = textos[1];
-                //sharpArray[con1] = textos[0];
+                object[] textos = item.Value.Split('_');
+                sharpArray[con1] = textos[0];
+                codigoArray[con1] = textos[1];
                 con1 += 1;
             }
             foreach (var item in cantidad)
@@ -92,53 +90,81 @@ namespace XmlPrueba
             {
                 iva = item.Value;
             }
-            foreach (var item in total)
+            foreach (var item in valTot)
             {
-                totalArray[con9] = item.Value;
+                totArray[con9] = item.Value;
                 con9 += 1;
             }
             foreach (var item in unique)
             {
-                dt.Rows.Add(sharpArray[conFina], codigoArray[conFina], cantidadArrray[conFina], descripcionArray[conFina], valunitArray[conFina], "IVA", iva, "Impuesto", "Descuento", totalArray[conFina]);
+                dt.Rows.Add(sharpArray[conFina], codigoArray[conFina], cantidadArrray[conFina], descripcionArray[conFina], valunitArray[conFina], "IVA", iva, "Descuento", totArray[conFina]);
                 conFina += 1;
             }
             foreach (var item in sub)
             {
                 subList.Add(item.Value);
             }
+            foreach (var item in totalPago)
+            {
+                TxtTotal.Text += (" "+item.Value);
+            }
             //dt.Rows.Add(priList[(0 + pri)], produList[(12 + val)], priList[(2 + pri)], produList[(10 + val)], produList[(13 + val)], "IVA", totaList[(3 + tot)], totaList[(2 + tot)], produList[(6 + val)], priList[(3 + pri)]);
             DataProducto.AutoGenerateColumns = true;
             DataProducto.ItemsSource = dt.DefaultView;
-            TBImpo.Text = Convert.ToString(subList[0]);
+            string sTotal = Convert.ToString(subList[0]);
+            double STotal = Convert.ToDouble(sTotal);
+            Sotal.Text = (sTotal);
             TIVA.Text = Convert.ToString(subList[1]);
-            txtIva.Text += " " + Convert.ToString(subList[2]) + "%";
+            txtIva.Text += " " + iva + "%";
+
         }
         private void LeerXML()
         {
             if (ruta != null)
             {
-                int rNameCont = 0;
                 XElement xelement = XElement.Load(ruta);
-                var proName = xelement.Descendants(cbc + "RegistrationName");
+                var proID = xelement.Elements(fe + "AccountingSupplierParty").Elements(fe + "Party").Elements().Elements(cbc + "Name");
+                var proNIT = xelement.Elements(fe + "AccountingSupplierParty").Elements(fe + "Party").Elements().Elements(cbc + "ID");
+                var proDir = xelement.Elements(fe + "AccountingSupplierParty").Elements(fe + "Party").Elements().Elements().Elements().Elements(cbc + "Line");
+                var cliID = xelement.Elements(fe + "AccountingCustomerParty").Elements(fe + "Party").Elements().Elements(cbc + "Name");
+                var cliNIT = xelement.Elements(fe + "AccountingCustomerParty").Elements(fe + "Party").Elements().Elements(cbc + "ID");
+                var cliDir = xelement.Elements(fe + "AccountingCustomerParty").Elements(fe + "Party").Elements().Elements().Elements().Elements(cbc + "Line");
                 var city = xelement.Descendants(cbc + "CityName");
                 var dire = xelement.Descendants(cbc + "Line");
                 var tele = xelement.Descendants(cbc + "Telephone");
-                var rName = xelement.Descendants(cbc + "RegistrationName");
-                var fiName = xelement.Descendants(cbc + "FirstName");
-                var faName = xelement.Descendants(cbc + "FamilyName");
-                var proID = xelement.Elements(cbc + "ID");
                 var unique = from el in xelement.Elements(fe + "InvoiceLine") select el;
                 var priNod = from el in xelement.Elements(fe + "InvoiceLine").Elements() select el;
                 var producto = from el in xelement.Elements(fe + "InvoiceLine").Elements().Elements() select el;
                 var total = from el in xelement.Elements(fe + "InvoiceLine").Elements().Elements().Elements() select el;
                 var legal = from el in xelement.Elements(fe + "LegalMonetaryTotal").Elements() select el;
-                
+                List<object> idList = new List<object>();
                 List<object> cityList = new List<object>();
                 List<object> direList = new List<object>();
                 List<object> teleList = new List<object>();
-                List<object> rNameList = new List<object>();
-                List<object> fiNameList = new List<object>();
-                List<object> faNameList = new List<object>();
+                foreach (var item in proID)//Nombre proveedor
+                {
+                    NombreTXT.Text = item.Value;
+                }
+                foreach (var item in proNIT)
+                {
+                    NITTXT.Text = item.Value;
+                }
+                foreach (var item in proDir)
+                {
+                    DirTXT.Text = item.Value;
+                }
+                foreach (var item in cliID)
+                {
+                    NombreTXT2.Text = item.Value;
+                }
+                foreach (var item in cliNIT)
+                {
+                    NITTXT2.Text = item.Value;
+                }
+                foreach (var item in cliDir)
+                {
+                    DirTXT2.Text = item.Value;
+                }
                 foreach (var item in legal)
                 {
                     legaList.Add(item.Value);
@@ -157,61 +183,12 @@ namespace XmlPrueba
                 }
                 foreach (var item in dire)
                 {
-                    direList.Add(item.Value);
                 }
                 foreach (var item in tele)
                 {
                     teleList.Add(item.Value);
                 }
-                foreach (var item in rName)
-                {
-                    rNameList.Add(item.Value);
-                    rNameCont += 1;
-                }
-                foreach (var item in fiName)
-                {
-                    fiNameList.Add(item.Value);
-                }
-                foreach (var item in faName)
-                {
-                    faNameList.Add(item.Value);
-                }
-                if (rNameCont == 0)
-                {
-                    if (Convert.ToString(faNameList[0]) != "")
-                    {
-                        NombreTXT.Text = Convert.ToString(fiNameList[0] + " " + faNameList[0]);
-                    }
-                    else
-                    {
-                        NombreTXT.Text = Convert.ToString(fiNameList[0]);
-                    }
-                }
-                else
-                {
-                    NombreTXT.Text = Convert.ToString(rNameList[0]);
-                }
-                //if (rNameCont == 1)
-                //{
-                //    if (Convert.ToString(faNameList[0]) != "")
-                //    {
-                //        NombreTXT2.Text = Convert.ToString(fiNameList[0] + " " + faNameList[0]);
-                //    }
-                //    else
-                //    {
-                //        NombreTXT2.Text = Convert.ToString(fiNameList[0]);
-                //    }
-                //}
-                //else
-                //{
-                //    NombreTXT2.Text = Convert.ToString(rNameList[1]);
-                //}
-                DirTXT.Text = (Convert.ToString(direList[0] + " - " + cityList[0]));
-                DirTXT2.Text = (Convert.ToString(direList[1] + " - " + cityList[1]));
-                //TelTXT.Text = (Convert.ToString(teleList[0]));
-                //TelTXT2.Text = (Convert.ToString(teleList[1]));
                 Llenar();
-
             }
             else
             {
